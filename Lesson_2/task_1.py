ec0f3f77069e0e37ede47bf226b78abcc38dc928
Denis <deniskovalev4791@gmail.com>
2020-01-25 23:39:27 +0300

    complete lessons

--- Lesson_2/task_1.py
+++ Lesson_2/task_1.py
@@ -2,3 +2,19 @@
 # Реализовать скрипт проверки типа данных каждого элемента.
 # Использовать функцию type() для проверки типа.
 # Элементы списка можно не запрашивать у пользователя, а указать явно, в программе.
+
+
+float_var = 12.443
+int_var = 12
+complex_var = complex(1, 2)
+string_var = 'this is a string'
+list_var = [1, 2, 3]
+tuple_var = tuple('tuple')
+set_var = set('this is a set var')
+bool_var = True
+list_of_types = [float_var, int_var, complex_var, string_var, list_var, tuple_var, set_var, bool_var]
+
+for variable in list_of_types:
+    print(type(variable))
+
+
--- Lesson_2/task_2.py
+++ Lesson_2/task_2.py
@@ -2,3 +2,17 @@
 # т.е. Значениями обмениваются элементы с индексами 0 и 1, 2 и 3 и т.д.
 # При нечетном количестве элементов последний сохранить на своем месте.
 # Для заполнения списка элементов необходимо использовать функцию input().
+
+list_from_input = [int(i) for i in input('Введите значения списка через пробел: ').split()]
+print(f'Ваш список {list_from_input}')
+
+i = 0
+
+while True:
+    if i >= len(list_from_input) - 1:
+        break
+
+    list_from_input[i], list_from_input[i+1] = list_from_input[i+1], list_from_input[i]
+    i += 2
+
+print(f'теперь массив выглядит вот так: {list_from_input}')
