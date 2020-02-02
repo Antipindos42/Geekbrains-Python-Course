d33da55665cb3601eeae2a345d7aae33e7792258
Denis <deniskovalev4791@gmail.com>
2020-02-02 15:26:04 +0300

    task 2 + some optimisation for task1

--- Lesson_3/task_1.py
+++ Lesson_3/task_1.py
@@ -1,12 +1,9 @@
-#homework lesson 2: task 1
+#homework lesson 3: task 1
 
 """
  Реализовать функцию, принимающую два числа (позиционные аргументы) и выполняющую их деление.
  Числа запрашивать у пользователя, предусмотреть обработку ситуации деления на ноль.
 """
-
-number = int(input('Что делим: '))
-divider = int(input('На что делим: '))
 
 
 def division(num, div):
@@ -18,4 +15,4 @@ def division(num, div):
     return result
 
 
-print(division(number, divider))
+print(division(int(input('Что делим: ')), int(input('На что делим: '))))
