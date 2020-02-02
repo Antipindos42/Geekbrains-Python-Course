5aba40d35b017b8a10f7d2005c388a1a783da62a
Denis <deniskovalev4791@gmail.com>
2020-02-02 15:42:14 +0300

    fixes for task 2

--- Lesson_3/task_2.py
+++ Lesson_3/task_2.py
@@ -1,4 +1,4 @@
-#homework lesson 3: task 2
+# homework lesson 3: task 2
 
 """
  Реализовать функцию, принимающую несколько параметров,
@@ -10,14 +10,15 @@
 
 
 def person(name, lastname, year_of_birth, city, email, phone):
-    return print(f'{name} {lastname}. {year_of_birth}, {city}. {email}, {phone}')
+    return print(f'Имя: {name} Фамилия: {lastname} Год рождения: {year_of_birth}'
+                 f'Город проживания: {city} Email: {email} Телефон: {phone}')
 
 
-print(person(
-    input('Имя: '),
-    input('Фамилия: '),
-    input('Год Рождения: '),
-    input('Город проживания: '),
-    input('email: '),
-    input('phone: '),
-))
+person(
+    name=input('Имя: '),
+    lastname=input('Фамилия: '),
+    year_of_birth=input('Год Рождения: '),
+    city=input('Город проживания: '),
+    email=input('email: '),
+    phone=input('phone: '),
+)
