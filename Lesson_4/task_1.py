a95d36d9b85ea4ade488d223651939690c8752f0
Denis <deniskovalev4791@gmail.com>
2020-02-03 19:12:49 +0300

    Task 1 (argparse realisation)

--- Lesson_4/task_1.py
+++ Lesson_4/task_1.py
@@ -5,7 +5,18 @@
 В расчете необходимо использовать формулу: (выработка в часах*ставка в час) + премия.
 Для выполнения расчета для конкретных значений необходимо запускать скрипт с параметрами.
 """
-
 from sys import argv
+import argparse
 
-print(argv)
+parser = argparse.ArgumentParser(description='Calculate salary')
+parser.add_argument('--rate', type=float)
+parser.add_argument('--hours', type=float)
+parser.add_argument('--prize', default=None, type=float)
+
+args = parser.parse_args(argv[1:])
+
+def salary(rate: float, hours: float, prize: float):
+    return rate * hours + prize
+
+
+print(f'Как работал, так и заработал: {salary(rate=args.rate, hours=args.hours, prize=args.hours)} руб')
