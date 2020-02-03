9d396858bda091805a217d4c6f26e56feecb9903
Denis <deniskovalev4791@gmail.com>
2020-02-03 20:03:15 +0300

    task 4

--- Lesson_4/task_4.py
+++ Lesson_4/task_4.py
@@ -8,3 +8,7 @@
 Пример исходного списка: [2, 2, 2, 7, 23, 1, 44, 44, 3, 2, 10, 7, 4, 11].
 Результат: [23, 1, 3, 10, 4, 11]
 """
+
+source_list = [1, 1, 2, 2, 33, 44, 3, 22, 12, 2, 32, 22, 33, 44]
+my_new_list = [i for i in source_list if source_list.count(i) < 2]
+print(my_new_list)
