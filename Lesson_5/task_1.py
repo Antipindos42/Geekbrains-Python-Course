66148a336540b9bb298b72b3e57c624b6174694c
Denis <deniskovalev4791@gmail.com>
2020-02-06 16:55:47 +0300

    task -1

--- Lesson_5/task_1.py
+++ Lesson_5/task_1.py
@@ -4,3 +4,13 @@
 Создать программно файл в текстовом формате, записать в него построчно данные, вводимые пользователем.
 Об окончании ввода данных свидетельствует пустая строка.
 """
+
+file = open('./files/task1.txt', 'w',  encoding='utf-8')
+while True:
+    string = input('Введите что-либо: ')
+    if not string:
+        file.close()
+        print('Выход')
+        break
+
+    file.write(f'{string}\n')
