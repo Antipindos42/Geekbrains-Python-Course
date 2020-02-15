0be8e161cdedcef2737221f5c6a57070d31da395
Denis <deniskovalev4791@gmail.com>
2020-02-15 23:58:50 +0300

    tasks 1,2,3

--- Lesson_8/task_2.py
+++ Lesson_8/task_2.py
@@ -6,3 +6,23 @@
 При вводе пользователем нуля в качестве делителя программа
 должна корректно обработать эту ситуацию и не завершиться с ошибкой.
 """
+
+
+class OopsDivisionByZero(Exception):
+    def __init__(self, txt):
+        self.txt = txt
+
+
+try:
+    devidend = int(input('Введите число, которое собираетесь разделить: '))
+    division = int(input('Введите число, НА которое собираетесь разделить: '))
+    if not division:
+        raise OopsDivisionByZero('Oops! division by zero')
+    print(f'Результат {devidend/division}')
+
+except ValueError:
+    print('вы ввели не числа')
+except OopsDivisionByZero as error:
+    print(error)
+
+
